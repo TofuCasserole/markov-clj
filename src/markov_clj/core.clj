ac0133511fe12705f8594d2360df4d9fa4359929
TofuCasserole <kristoffe.zehr@gmail.com>
2015-07-08 11:05:03 -0500

    removed current HMM implementation.

--- src/markov_clj/core.clj
+++ src/markov_clj/core.clj
@@ -1,19 +1,2 @@
 (ns markov-clj.core
   (:require [markov-clj.hmm :as hmm]))
-
-(def model
-  "an example hidden markov model"
-  (hmm/->HiddenMarkovModel #{"Healthy" "Fever"}
-                                    #{"Normal" "Cold" "Dizzy"}
-                                    {"Healthy" 0.6
-                                     "Fever"   0.4}
-                                    {"Healthy" {"Healthy" 0.7
-                                                "Fever"   0.3}
-                                     "Fever"   {"Healthy" 0.4
-                                                "Fever"   0.6}}
-                                    {"Healthy" {"Normal" 0.5
-                                                "Cold"   0.4
-                                                "Dizzy"  0.1}
-                                     "Fever"   {"Normal" 0.1
-                                                "Cold"   0.3
-                                                "Dizzy"  0.6}}))
