f1e1904b204cd7394ee142d5e48f6e7c7d5519e2
TofuCasserole <kristoffe.zehr@gmail.com>
2015-07-10 23:39:40 -0500

    starting work on forward algorithm for matrix hmm

--- src/markov_clj/hmm.clj
+++ src/markov_clj/hmm.clj
@@ -3,3 +3,18 @@
 
 (defprotocol HiddenMarkovModel
   (forward-prob [model observations]))
+
+(defn- norm-prob-vec [v]
+  (let [vecsum (esum v)]
+    ))
+
+(defrecord MatrixHMM [states observables init-p trans-p emit-p]
+  HiddenMarkovModel
+  (forward-prob
+   [model observations]
+   (reduce (fn [prev-prob ])
+           (:init-p model)
+           (map (fn [obs]
+                  (* (diagonal-matrix (get-row (:emit-p model) obs))
+                     (:trans-p model)))
+                observations))))
---
+++ test/markov_clj/hmm_test.clj
@@ -0,0 +1,3 @@
+(ns markov-clj.hmm.test
+  (:require [clojure.test :refer :all]
+            [markov-clj.hmm :refer :all]))
