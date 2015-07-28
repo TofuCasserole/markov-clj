83da4f7b8a03343280a364a6a79d3e9958a43ff2
TofuCasserole <kristoffe.zehr@gmail.com>
2015-07-28 15:44:42 -0500

    Implemented forward backward algorithm
    Broke out the common code in the forward and backward portions of the forward backward algorithm.

--- src/markov_clj/hmm.clj
+++ src/markov_clj/hmm.clj
@@ -1,21 +1,52 @@
 (ns markov-clj.hmm
   (:require [clojure.core.matrix :refer :all]))
 
-(defprotocol HiddenMarkovModel
-  (forward-prob [model observations]))
+(defrecord MatrixHMM [states observables init-p trans-p emit-p])
 
-(defn- norm-prob-vec [v]
-  (map (partial * (/ (esum v))) v))
+(defn norm-prob-vec [v]
+  (array (map (partial * (/ (esum v))) v)))
 
-(defn- create-xform [model observation]
-  (mmul (:trans-p model)
-        (diagonal-matrix (get-row (:emit-p model) observation))))
+(defn fb-reduction [prev-prob xform]
+  (norm-prob-vec (mmul prev-prob xform)))
 
-(defrecord MatrixHMM [init-p trans-p emit-p]
-  HiddenMarkovModel
-  (forward-prob
-   [model observations]
-   (reduce (comp norm-prob-vec mmul)
-           (:init-p model)
-           (map (partial create-xform model)
-                observations))))
+(defn emission-matrix [emit-p observation]
+  (diagonal-matrix (get-row emit-p observation)))
+
+(defn initial-prob [init-p emit-p observation]
+  (norm-prob-vec (mmul (emission-matrix emit-p observation) init-p)))
+
+(defn create-xform [trans-p emit-p]
+  #(mmul trans-p (emission-matrix emit-p %)))
+
+(defn fb-process [init-p trans-p emit-p observations]
+  (reductions fb-reduction
+              (initial-prob init-p emit-p (first observations))
+              (map (create-xform trans-p emit-p) (rest observations))))
+
+(defn forward-prob
+  [model observations]
+  (fb-process (:init-p model) (:trans-p model) (:emit-p model) observations))
+
+(defn backward-prob
+  [model observations]
+  (reverse
+   (fb-process (array (repeat (count (:observables model)) 1))
+               (transpose (:trans-p model))
+               (:emit-p model)
+               (rseq observations))))
+
+(defn forward-backward
+  [model observations]
+  (let [fwd  (forward-prob model observations)
+        bkwd (backward-prob model observations)]
+    (map (comp norm-prob-vec mul) fwd bkwd)))
+
+(def example-hmm (->MatrixHMM [0 1]
+                              [0 1]
+                              [0.5 0.5]
+                              [[0.7 0.3]
+                               [0.3 0.7]]
+                              [[0.9 0.2]
+                               [0.1 0.8]]))
+
+(forward-backward example-hmm [0 0 1 0 0])
